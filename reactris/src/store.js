3be52ce497f9377c68bf2c21eaf902f1fbb0510d
honmanyau <honmanyau@users.noreply.github.com>
2018-03-21 00:21:45 +1100

    Cleaned up after renaming.

--- reactris/src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import { circletReducer as circlet } from 'circlet';
-import reactris from './reducers';
-
-const reducer = combineReducers({ circlet, reactris });
-const store = createStore(reducer);
-
-export default store;

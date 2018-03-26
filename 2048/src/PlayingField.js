0491fa2ee57c3864959e658172cca3e94775db01
honmanyau <honmanyau@users.noreply.github.com>
2018-03-26 17:14:42 +1100

    Added different colours for tiles up to 2048.

--- 2048/src/PlayingField.js
+++ 2048/src/PlayingField.js
@@ -5,6 +5,18 @@ import { MEDIA_MAX_WIDTH } from './constants';
 
 
 
+const COLOURS = [
+  '#b9bcff',
+  '#bae1ff',
+  '#a8e6cf', '#c2e9c8',
+  '#dcedc1', '#ede0bc',
+  '#ffd4b7', '#ffbfae',
+  '#ffaaa5', '#ff9a9c',
+  '#ff8b94'
+];
+
+const NUMBERS = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192];
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -23,13 +35,16 @@ const Tile = styled.div`
   border-radius: 4px;
   font-size: 32px;
   color: white;
-  background: ${props => props.val ? '#ff8b94' : '#CCC'};
+  background: ${props => props.val ? props.colour : '#CCC'};
 `;
 
 const playingField = ({ field }) => {
   const flattenedField = field.join(',').split(',');
   const tiles = flattenedField.map((val, index) => {
-    return <Tile key={index} val={val}>{val}</Tile>;
+    const num = Number(val);
+    const colour = num > 2048 ? '#ff8b94' : COLOURS[NUMBERS.indexOf(num)];
+
+    return <Tile key={index} val={val} colour={colour}>{val}</Tile>;
   });
 
   return(
